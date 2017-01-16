c5e8e88cc83931eaba74335605670c2fd3bd8271
Nikita Loges <git@nikita-loges.de>
2017-01-16 16:07:24 +0100

    remove custom chain loader

--- src/ShapecodeTwigStringLoaderBundle.php
+++ src/ShapecodeTwigStringLoaderBundle.php
@@ -2,25 +2,14 @@
 
 namespace Shapecode\Bundle\TwigStringLoaderBundle;
 
-use Shapecode\Bundle\TwigStringLoaderBundle\DependencyInjection\Compiler\TwigStringLoaderPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class ShapecodeTwigStringLoaderBundle
+ *
  * @package Shapecode\Bundle\TwigStringLoaderBundle
- * @author Nikita Loges
+ * @author  Nikita Loges
  */
 class ShapecodeTwigStringLoaderBundle extends Bundle
 {
-
-    /**
-     * @inheritdoc
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new TwigStringLoaderPass());
-    }
 }
