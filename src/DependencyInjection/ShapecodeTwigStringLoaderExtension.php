301724db5108271f040035f11616c17f78b78158
Nikita Loges <git@loges.one>
2019-11-04 20:21:11 +0100

    cq

--- src/DependencyInjection/ShapecodeTwigStringLoaderExtension.php
+++ src/DependencyInjection/ShapecodeTwigStringLoaderExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Shapecode\Bundle\TwigStringLoaderBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
@@ -7,19 +9,12 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * Class ShapecodeTwigStringLoaderExtension
- *
- * @package Shapecode\Bundle\TwigStringLoaderBundle\DependencyInjection
- * @author  Nikita Loges
- */
 class ShapecodeTwigStringLoaderExtension extends Extension
 {
-
     /**
      * @inheritdoc
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container) : void
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
--- src/ShapecodeTwigStringLoaderBundle.php
+++ src/ShapecodeTwigStringLoaderBundle.php
@@ -1,15 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Shapecode\Bundle\TwigStringLoaderBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class ShapecodeTwigStringLoaderBundle
- *
- * @package Shapecode\Bundle\TwigStringLoaderBundle
- * @author  Nikita Loges
- */
 class ShapecodeTwigStringLoaderBundle extends Bundle
 {
 }
